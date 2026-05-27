889a1824c3988cb8932d0c98bdb47a7e36bb6a8c
Canis Ouambo <118950061+OuamboC@users.noreply.github.com>
2026-05-27 23:06:53 +0200

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Declare a dict to store nums
+        result = dict()
+        # Loop through nums
+        for i in range(len(nums)):
+            # Check if target - nums[i] in result 
+            if target - nums[i] in result:
+                return [result.get(target - nums[i]), i]
+            else:
+                result[nums[i]] = i
+        return None
+        
